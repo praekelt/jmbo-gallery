5f87c2f7aa3933935ad31a7744e8e84646b18d48
hedleyroos <hedleyroos@gmail.com>
2012-08-17 10:57:43 +0200

    Ported code from jmbo-foundry

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns(
-    'gallery.views',
-    url(r'^list/$', 'object_list', name='gallery_object_list'),
-    url(r'^(?P<slug>[\w-]+)/$', 'object_detail', name='gallery_object_detail'),
-    url(r'^item/ajax/galleriffic/(?P<slug>[\w-]+)/$', 'gallery_item_ajax_galleriffic', name='gallery_item_ajax_galleriffic'),
-)
+#urlpatterns = patterns(
+#    'gallery.views',
+#)
+urlpatterns = ()
