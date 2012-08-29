b31cf7dbff38364d3f35d05a8c88886d4d4d2810
hedleyroos <hedleyroos@gmail.com>
2012-08-29 14:54:46 +0200

    Videos launch the native player on mobile devices

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -5,6 +5,7 @@ next
 ----
 #. Refactor embedded video so it can be used in a gallery.
 #. Finer grained photo sizes. Run `jmbo-foundry` load_photosizes to activate these new photo sizes.
+#. Videos launch the native player on mobile devices.
 
 0.1
 ---
--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,13 @@
+import os
+import re
+import urllib2
+from tempfile import mkdtemp
+
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.core.files import File
 
+from photologue.models import Image
 from jmbo.models import ModelBase
 
 
@@ -35,11 +42,42 @@ class GalleryImage(GalleryItem):
 
 class VideoEmbed(GalleryItem):
     embed = models.TextField(
-        help_text="""Embedding markup as supplied by Youtube or Vimeo.""")
+        help_text="""Embedding markup as supplied by Youtube.""")
 
     class Meta:
         verbose_name = "Video embed"
         verbose_name_plural = "Video embeds"
+
+    @property
+    def youtube_id(self):
+        """Extract and return Youtube video id"""
+        m = re.search(r'/embed/([A-Za-z0-9\-=_]*)"', self.embed)
+        if m:
+            return m.group(1)
+        return ''
+    
+    def save(self, *args, **kwargs):
+        """Automatically set image"""
+        url = "http://img.youtube.com/vi/%s/0.jpg" % self.youtube_id
+        try:
+            response = urllib2.urlopen(url)
+        except Exception, e:
+            # Blindly catch exceptions
+            pass
+        else:            
+            # Jump through filesystem hoop to please photologue
+            filename = self.youtube_id + '.jpg'
+            filepath = os.path.join(mkdtemp(), filename)
+            fp = open(filepath, 'wb')
+            try:
+                fp.write(response.read())
+            finally:
+                fp.close()
+            image = File(open(filepath, 'rb'))
+            image.name = filename
+            self.image = image
+
+        super(VideoEmbed, self).save(*args, **kwargs)
 
 
 class VideoFile(GalleryItem):
