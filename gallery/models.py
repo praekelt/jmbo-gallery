d0fed6901de6a67b2e48bc67cba95d6a717427ec
Rizmari Versfeld <rizmari@praekelt.com>
2012-08-30 10:31:08 +0200

    first try at overlaying a button on video embed image'

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,14 @@
+import os
+
 from django.core.urlresolvers import reverse
 from django.db import models
 
 from jmbo.models import ModelBase
+
+from preferences import Preferences
+
+from PIL import Image, ImageDraw
+
 
 class Gallery(ModelBase):
     class Meta():
@@ -33,9 +40,48 @@ class VideoEmbed(GalleryItem):
     class Meta():
         verbose_name = "Video embed"
         verbose_name_plural = "Video embeds"
+ 
+    def save(self, *args, **kwargs):
+        pref = GalleryPreference.objects.all()
+        if pref:
+            if self.image:
+                image = Image.open(self.image.url)
+                image_overlay = Image.open(pref.video_play_image.url)
+                # downsize image_overlay if it is larger than image
+                w1, h1 = image.size
+                w2, h2 = image_overlay.size
+                if w2 > w1 or h2 > h1:
+                    ratio1 = w1 / float(h1)
+                    ratio2 = w2 / float(h2)
+                    if ratio1 > ratio2:
+                        resize_fract = h1 / float(h2)
+                    else:
+                        resize_fract = w1 / float(w2)
+                    
+                    image_overlay.resize(w2 * resize_fract, h2 * resize_fract, Image.ANTIALIAS)
+                
+                image.paste(image_overlay, ((w1 - w2) / 2.0, (h1 - h2) / 2.0))
+                image.save("%s_b%s" % os.path.splitext(self.image.url))
+                self.image = "%s_b%s" % os.path.splitext(self.image.name)
+                
+        else:
+            raise GalleryPreference.DoesNotExist("The video play overlay image is required.")
+
+        super(VideoEmbed, self).save(*args, **kwargs)
 
 class VideoFile(GalleryItem):
     file = models.FileField(upload_to='content/videofile')
     class Meta():
         verbose_name = "Video file"
         verbose_name_plural = "Video files"
+
+
+class GalleryPreferences(Preferences):
+    __module__ = 'preferences.models'
+
+    video_play_image = models.ImageField(
+        upload_to="preferences",
+        help_text="The play button image that is overlaid on a video image"
+    )
+    
+    
